cf9576679e35a4ac97df2277e8c5c57a091ed86d
Abiodun Oni <abiodun.oni@bankly.ng>
2024-01-10 07:55:01 +0100

    creating forst appwithnswift final class

--- MadLib2/ViewController.swift
+++ MadLib2/ViewController.swift
@@ -8,12 +8,21 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var nameTextfield: UITextField!
+    @IBOutlet weak var quantityTextField: UITextField!
+    @IBOutlet weak var itemTextField: UITextField!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    @IBAction func onPressed(_ sender: Any) {
+        let greetings : String = "Hello,  \(nameTextfield.text ?? "Kola") you have placed an order for \(quantityTextField.text ?? "0") \(itemTextField.text ?? "nothing") "
+        greetingTextfield.text = greetings
+        
+    }
+    
+    
+    @IBOutlet weak var greetingTextfield: UILabel!
 }
 
